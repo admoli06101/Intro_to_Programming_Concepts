41986fe052fb35018dd452377c72021cac1f91cb
admoli06101 <admoli06@gmail.com>
2026-01-20 17:57:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
 print("Aiden Molinary")
+# Using the pound/hashtag symbol is a way to do comments.
+print('Hello, how are you?')
+
+name = "Scooby" #variables unlike other languages don't have a type.
+# in python = is "get", == "double get sign"
+
+# int (whole number positive/negative)
+# double (double the float or double float)
+# float (numbers with decimal)
+# char (character or one single character)
+# string (words or group of characters together)
+# boolean (True and false, 1's and 0's)
+
+age = 23
+
+# when using words in python, use "", when not using words, you do not use "", such as numbers
+
+temperature = 98.7
+# float
+
+ # types of printing
+print ("I am {} years old, and the temperature is {} degrees".format(age, temperature))
+# {} Are placeholders and make sure to label them in order
+# "*args:" is the 'in tell a sense' putting the argument into the print statement
+
+
+# input
+friendName = input("What is your name?\n ")
+# camal case, first letter of first word lowercase, last word first letter uppercase
+# "\n" new line, "\t" tab, "\s" space
+print(friendName)
+print("The new friend name is ", friendName)
+
+fage = int(input("What is your favorite age\n "))
+print(fage)
+# "fage" is friend's age
+
+total = 52 + 78
+print(total)
+
+agesTotal = fage + age
+print(agesTotal)
+
+# Modulus could do math with %'s, which will give you the remainder number
+modulusTotal = 5 % 2
+print(modulusTotal)
+
