4cdc33662025e5e4a8cd4cc9785b33d6bc7a5e70
admoli06101 <admoli06@gmail.com>
2026-02-03 16:30:11 -0500

    Create VA2.py

---
+++ VA2.py
@@ -0,0 +1,37 @@
+#That we want to multiply two numbers
+numMulti = int(input("Enter a number: "))
+numMulti2 = int(input("Enter another number: "))
+answerMulti = numMulti * numMulti2
+print("The answer to your query is", answerMulti)
+
+#That we want to divide two numbers
+numDiv = int(input("Enter a number: "))
+numDiv2 = int(input("Enter another number: "))
+answerDiv = numDiv / numDiv2
+print("The answer to your query is", answerDiv)
+
+#That we want to divide two numbers without the remainder
+numNoRemain = int(input("Enter a number: "))
+numNoRemain2 = int(input("Enter another number: "))
+answerNoRemain = numNoRemain // numNoRemain2
+print("The answer to your query is", answerNoRemain)
+
+#That we want the remainder of two numbers 'modulus'
+numRemain = int(input("Enter a number: "))
+numRemain2 = int(input("Enter another number: "))
+answerRemain = numRemain % numRemain2
+print("The answer to your query is", answerNoRemain)
+
+#That we want to add two numbers
+numSum = int(input("Enter a number: "))
+numSum2 = int(input("Enter another number: "))
+answerSum = numSum + numSum2
+print("The answer to your query is", answerSum)
+
+#That we want to subtract two numbers
+numSub = int(input("Enter a number: "))
+numSub2 = int(input("Enter another number: "))
+answerSub = numSub - numSub2
+print("The answer to your query is", answerSub)
+
+print("Aiden Molinary")
