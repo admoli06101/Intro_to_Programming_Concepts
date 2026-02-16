29588f4d410eb065f72917589360602601895731
admoli06101 <admoli06@gmail.com>
2026-02-15 20:49:58 -0500

    Create Collections Assignment.py

---
+++ Collections Assignment.py
@@ -0,0 +1,200 @@
+studentName = input("What is the student's name?\n")
+print(studentName)
+
+inputGrades = int(input("Please enter the grade"))
+gradeList = []
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+print(gradeList)
+print(len(gradeList))
+sum = gradeList[0] + gradeList[1] + gradeList[2] + gradeList[3] + gradeList[4]
+avg = sum / len(gradeList)
+print (avg)
+
+print ("Grade C")
+
+
+
+studentName = input("What is the next student's name?\n")
+print(studentName)
+
+inputGrades = int(input("Please enter the grade"))
+gradeList = []
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+print(gradeList)
+print(len(gradeList))
+sum = gradeList[0] + gradeList[1] + gradeList[2] + gradeList[3] + gradeList[4]
+avg = sum / len(gradeList)
+print (avg)
+
+print ("Grade B")
+
+
+
+studentName = input("What is the next student's name?\n")
+print(studentName)
+
+inputGrades = int(input("Please enter the grade"))
+gradeList = []
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+print(gradeList)
+print(len(gradeList))
+sum = gradeList[0] + gradeList[1] + gradeList[2] + gradeList[3] + gradeList[4]
+avg = sum / len(gradeList)
+print (avg)
+
+print ("Grade C")
+
+
+
+studentName = input("What is the next student's name?\n")
+print(studentName)
+
+inputGrades = int(input("Please enter the grade"))
+gradeList = []
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+print(gradeList)
+print(len(gradeList))
+sum = gradeList[0] + gradeList[1] + gradeList[2] + gradeList[3] + gradeList[4]
+avg = sum / len(gradeList)
+print (avg)
+
+print ("Grade D")
+
+
+
+studentName = input("What is the next student's name?\n")
+print(studentName)
+
+inputGrades = int(input("Please enter the grade"))
+gradeList = []
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+print(gradeList)
+print(len(gradeList))
+sum = gradeList[0] + gradeList[1] + gradeList[2] + gradeList[3] + gradeList[4]
+avg = sum / len(gradeList)
+print (avg)
+
+print ("Grade A")
+
+
+
+studentName = input("What is the next student's name?\n")
+print(studentName)
+
+inputGrades = int(input("Please enter the grade"))
+gradeList = []
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+print(gradeList)
+print(len(gradeList))
+sum = gradeList[0] + gradeList[1] + gradeList[2] + gradeList[3] + gradeList[4]
+avg = sum / len(gradeList)
+print (avg)
+
+print ("Grade C")
+
+
+
+studentName = input("What is the last student's name?\n")
+print(studentName)
+
+inputGrades = int(input("Please enter the grade"))
+gradeList = []
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+inputGrades = int(input("Please enter the next grade"))
+gradeList.append(inputGrades)
+
+print(gradeList)
+print(len(gradeList))
+sum = gradeList[0] + gradeList[1] + gradeList[2] + gradeList[3] + gradeList[4]
+avg = sum / len(gradeList)
+print (avg)
+
+print ("Grade B")
