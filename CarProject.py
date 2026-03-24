26caea2991bc5a95942d4dd78f9547050319ae12
admoli06101 <admoli06@gmail.com>
2026-03-24 16:37:42 -0400

    Create CarProject.py

---
+++ CarProject.py
@@ -0,0 +1,22 @@
+#Constructor that helps to find the class, which is __init__, "Init" stands for Initialize
+#class Vehicle:
+    #def __init__(self, brand, type, doorCount, model):
+        #self.brand = brand
+        #self. type = type
+        #self. doorCount = doorCount
+        #self. model = model
+
+#khoaCar = Vehicle("Tesla", "Truck", 2, "Cyber Truck")
+#johanCar = Vehicle("BMW", "Sports Car", "2", "M4")
+
+
+#print(khoaCar.brand)
+#print(johanCar.doorCount)
+
+class Person:
+    def __init__(self,age, name, major):
+        self.age = age
+        self.name = name
+        self.major = major
+
+class Classroom:
