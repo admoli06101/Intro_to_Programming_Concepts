2b73ab9c113a5f67cc57684ce005fde116b50948
admoli06101 <admoli06@gmail.com>
2026-04-05 21:31:00 -0400

    Extra Credit

--- DictionaryMenu(FinalExamExtraCredit).py
+++ DictionaryMenu(FinalExamExtraCredit).py
@@ -1,5 +1,24 @@
+menu = {
+    "burger": 5.99,
+    "pizza": 8.99,
+    "salad": 4.99,
+    "soda": 1.99,
+    "fries": 2.99
+}
 
+total = 0.0
 
+while True:
+    item = input()
 
+    if item == "done":
+        break
 
+    if item in menu:
+        total = total + menu[item]
+    else:
+        print("Item not found")
 
+print("Total amount:", total)
+
+print("Aiden Molinary")
